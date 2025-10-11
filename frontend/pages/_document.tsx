c63cda139b23db7d884459e51390a8d906bf44f7
Ryan Joseph <ryan@electricsheep.co>
2025-10-10 17:56:01 -0700

    Display fixes

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=Fascinate&family=Limelight&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Limelight&display=swap" 
           rel="stylesheet" 
         />
       </Head>
